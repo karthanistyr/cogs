5669b6690785677d4759bea6029d37b6d7380b33
Antoine Mazeas <antoine@karthanis.net>
2018-01-12 18:06:27 +0900

    Try identifying the command issuer

--- gw2/gw2.py
+++ gw2/gw2.py
@@ -1,19 +1,20 @@
 import discord
-#import requests
+import requests
 from discord.ext import commands
 
 class gw2:
     """My custom cog that does stuff!"""
 
     def __init__(self, bot):
+        self.commandIssuer = ctx.message.author
         self.bot = bot
 
     @commands.command()
-    async def achievements(self):
+    async def characters(self):
         """This does stuff!"""
 
         #Your code will go here
-        await self.bot.say("I can do stuff!")
+        await self.bot.say("I can do stuff to" + commandIssue.mention  + "!")
 
 def setup(bot):
     bot.add_cog(gw2(bot))
