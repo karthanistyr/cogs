82ba60add269553cea51dda36b671dd0a8a8b65f
Antoine Mazeas <antoine@karthanis.net>
2018-01-12 19:35:19 +0900

    API Key management first try

--- gw2/gw2.py
+++ gw2/gw2.py
@@ -1,5 +1,6 @@
 import discord
 import requests
+from .utils.dataIO import fileIO
 from discord.ext import commands
 
 class gw2:
@@ -7,14 +8,52 @@ class gw2:
 
     def __init__(self, bot):
         self.bot = bot
+        with open("data/gw2/localised_strings.json", "r") as strngs:
+            self.strings = json.load(strngs)
+        
+        #default (hardcode) to french locale for now
+        self.locale = "fr"
 
     @commands.command(pass_context=True)
     async def characters(self, ctx):
         """This does stuff!"""
 
         #Your code will go here
-        print(self)
-        await self.bot.say("I can do stuff to" + ctx.message.author.mention  + "!")
+        await self.bot.say("I can do stuff to " + ctx.message.author.mention  + "!")
+
+    @commands.command(pass_context=True)
+    async def storekey(self, ctx, apiKey=None):
+        if(apiKey == None):
+            await self.bot.say(self.strings[self.locale]["no_key_passed"]
+            return
+        
+        keys = loadApiKeys()
+
+        if(ctx.message.author.id in keys):
+            await self.bot.say(self.strings[self.locale]["key_override_warning"]
+        else:
+            keys[ctx.message.author.id] = apiKey
+
+        writeKeys(keys)
+    
+    @commands.command(pass_context=True)
+    async def deletekey(self, ctx):
+    
+    keys = loadApiKeys()
+
+    if(ctx.message.author.id in keys):
+        del keys[ctx.message.author.id]
+
+    writeKeys(keys)
+
+def loadApisKeys():
+    with open("data/gw2/api_keys.json", "r") as key_file:
+        keys = json.load(key_file)
+    return keys
+
+def writeKeys(keys):
+    with open("data/gw2/api_keys.json", "w") as key_file:
+        json.dump(keys, key_file)
 
 def setup(bot):
     bot.add_cog(gw2(bot))
