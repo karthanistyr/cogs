d86aca8dbffa710bdb5332c62aa539e5fc3babf2
Antoine Mazeas <antoine@karthanis.net>
2018-01-12 23:18:23 +0900

    modularise gw2 api client

--- gw2/gw2.py
+++ gw2/gw2.py
@@ -3,11 +3,32 @@ import requests
 from .utils.dataIO import fileIO
 from discord.ext import commands
 
+class gw2_api_client:
+    def __init__(self):
+        self.root_api_endpoint = "https://api.guildwars2.com"
+
+    def get_request(endpoint, arguments, api_key=None):
+        complete_endpoint = self.root_api_endpoint = endpoint
+
+        if(api_key is not None):
+            headers = {"Authorization": "Bearer {}".format(api_key)}
+
+        get_response = requests.get(complete_endpoint, params=arguments, headers=headers)
+
+        if(get_response.status_code == 200):
+            return get_response.json()
+
+    def get_characters(self, api_key):
+        ep = "/v2/characters"
+
+        char_data = get_request(ep, None, api_key)
+        return char_data
+
 class gw2:
     def __init__(self, bot):
         self.bot = bot
         self.strings = fileIO("data/gw2/localised_strings.json", "load")
-        
+
         #default (hardcode) to french locale for now
         self.locale = "fr"
         self.strngs = self.strings[self.locale]
@@ -24,13 +45,6 @@ class gw2:
         if(userId in keys):
             return keys[userId]
 
-    def getRequest(self, endpoint, apiKey):
-        rootEndpoint = "https://api.guildwars2.com"
-        authorizationHeader = {"Authorization": "Bearer " + apiKey}
-
-        r = requests.get(rootEndpoint + endpoint, headers=authorizationHeader)
-        return r.json()
-
     @commands.command(pass_context=True)
     async def characters(self, ctx):
         apiKey = self.getUserKey(ctx.message.author.id)
@@ -38,20 +52,17 @@ class gw2:
             await self.bot.say(self.strings[self.locale]["no_key_exists"])
             return
 
-        charData = self.getRequest("/v2/characters", apiKey)
-        
-        chars = ""
-        for char in charData:
-            chars += char + "\n"
-            
+        api_client = gw2_api_client()
+
+        chars = api_client.get_characters(apiKey)
         await self.bot.say(chars)
-        
+
     @commands.command(pass_context=True)
     async def storekey(self, ctx, apiKey=None):
         if(apiKey is None):
             await self.bot.say(self.strings[self.locale]["no_key_passed"])
             return
-        
+
         keys = self.loadApiKeys()
 
         if(ctx.message.author.id in keys):
@@ -62,15 +73,15 @@ class gw2:
         self.writeKeys(keys)
 
         await self.bot.say(self.strings[self.locale]["command_completed"])
-    
+
     @commands.command(pass_context=True)
     async def deletekey(self, ctx):
-    
+
         keys = self.loadApiKeys()
 
         if(ctx.message.author.id in keys):
             del keys[ctx.message.author.id]
-        
+
         self.writeKeys(keys)
         await self.bot.say(self.strings[self.locale]["command_completed"])
 
