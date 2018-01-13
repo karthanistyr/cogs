9f6185dc301c43e93faf21a9f4cd5e4dc7b584a0
Antoine Mazeas <antoine@karthanis.net>
2018-01-13 10:32:01 +0900

    support for daily quests

--- gw2/gw2.py
+++ gw2/gw2.py
@@ -1,5 +1,6 @@
 import discord
 import requests
+from enum import Enum
 from .utils.dataIO import fileIO
 from discord.ext import commands
 
@@ -24,14 +25,31 @@ class gw2_api_client:
         char_data = self.get_request(ep, None, api_key)
         return char_data
 
+    def get_dailies(self, tomorrow: bool=None):
+        ep_today = "/v2/achievements"
+        ep_tomorrow = ep_today + "/tomorrow"
+
+        dailies_data = self.get_request(ep_tomorrow if tomorrow else ep_today, None)
+        return dailies_data
+
+    def get_daily_quest_details(self, ids, lang=None)
+        ep_daily_details = "/v2/achievements"
+        args = {"ids": ids}
+
+        if(lang is not None):
+            args["lang"] = lang
+
+        daily_details = self.get_request(ep_daily_details, args)
+        return daily_details
+
 class gw2:
     def __init__(self, bot):
         self.bot = bot
-        self.strings = fileIO("data/gw2/localised_strings.json", "load")
+        self.locales = fileIO("data/gw2/localised_strings.json", "load")
 
         #default (hardcode) to french locale for now
         self.locale = "fr"
-        self.strngs = self.strings[self.locale]
+        self.strings = self.locales[self.locale]
 
     def loadApiKeys(self):
         keys = fileIO("data/gw2/api_keys.json", "load")
@@ -49,37 +67,57 @@ class gw2:
     async def characters(self, ctx):
         apiKey = self.getUserKey(ctx.message.author.id)
         if(apiKey is None):
-            await self.bot.say(self.strings[self.locale]["no_key_exists"])
+            await self.bot.say(self.strings["no_key_exists"])
             return
 
         api_client = gw2_api_client()
 
         chars = api_client.get_characters(apiKey)
 
-        em = discord.Embed(title=self.strngs["characters_title"].format(ctx.message.author.name))
+        em = discord.Embed(title=self.strings["characters_title"].format(ctx.message.author.name))
         for char in chars:
             em.add_field(name="char", value=char, inline=True)
 
-        #response = self.bot.loop.run_in_executor(None, em)
-        #result = await asyncio.wait_for(response, timeout=10)
         await self.bot.say(embed=em)
+
+    @commands.command()
+    async def dailies(self, category, tomorrow=None):
+        if(category is None):
+            await self.bot.say(self.strings["missing_parameter"].format("category"))
+            return
+
+        valid_categories = {"pve", "pvp", "wvw", "fractals", "special"}
+        if(category not in valid_categories):
+            await self.bot.say(self.strings["invalid_parameter"].format(category, "category"))
+            return
+
+        api_client = gw2_api_client()
+        dailies = api_client.get_dailies(True if (tomorrow == "tomorrow") else False)
+
+        daily_ids = {}
+        for daily in dailies[category]:
+            daily_ids.append(daily["id"])
+
+        daily_details = api_client.get_daily_quest_details(",".join(daily_ids), self.locale)
+
+        await self.bot.say(daily_details)
 
     @commands.command(pass_context=True)
     async def storekey(self, ctx, apiKey=None):
         if(apiKey is None):
-            await self.bot.say(self.strings[self.locale]["no_key_passed"])
+            await self.bot.say(self.strings["no_key_passed"])
             return
 
         keys = self.loadApiKeys()
 
         if(ctx.message.author.id in keys):
-            await self.bot.say(self.strings[self.locale]["key_exists_warning"])
+            await self.bot.say(self.strings["key_exists_warning"])
         else:
             keys[ctx.message.author.id] = apiKey
 
         self.writeKeys(keys)
 
-        await self.bot.say(self.strings[self.locale]["command_completed"])
+        await self.bot.say(self.strings["command_completed"])
 
     @commands.command(pass_context=True)
     async def deletekey(self, ctx):
@@ -90,7 +128,7 @@ class gw2:
             del keys[ctx.message.author.id]
 
         self.writeKeys(keys)
-        await self.bot.say(self.strings[self.locale]["command_completed"])
+        await self.bot.say(self.strings["command_completed"])
 
 def setup(bot):
     bot.add_cog(gw2(bot))
