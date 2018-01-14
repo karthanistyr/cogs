4bdb5f9daacea3f4e43d61617a0f9ced3e72d71d
Antoine Mazeas <antoine@karthanis.net>
2018-01-14 18:18:38 +0900

    split api and bot scripts

--- gw2/gw2.py
+++ gw2/gw2.py
@@ -1,7 +1,119 @@
 import discord
+import requests
+from enum import Enum
 from .utils.dataIO import fileIO
 from discord.ext import commands
-import gw2_api_client
+
+#from https://wiki.guildwars2.com/wiki/Template:Rarity
+class gw2_constants:
+    def __init__(self):
+        self.rarity_colour = {
+                "Junk": 0xAAAAAA,
+                "Fine": 0x62A4DA,
+                "Masterwork": 0x1a9306,
+                "Rare": 0xfcd00b,
+                "Exotic": 0xffa405,
+                "Ascended": 0xfb3e8d,
+                "Legendary": 0x4C139D,
+                "Basic": 0xFFFFFF }
+
+class gw2_api_client:
+    def __init__(self):
+        self.root_api_endpoint = "https://api.guildwars2.com"
+
+    def get_request(self, endpoint, arguments, api_key=None):
+        complete_endpoint = self.root_api_endpoint + endpoint
+        headers = None
+
+        if(api_key is not None):
+            headers = {"Authorization": "Bearer {}".format(api_key)}
+
+        get_response = requests.get(complete_endpoint, params=arguments, headers=headers)
+
+        if(get_response.status_code == 200):
+            return get_response.json()
+
+    def get_characters(self, api_key):
+        ep = "/v2/characters"
+
+        char_data = self.get_request(ep, None, api_key)
+        return char_data
+
+    def get_dailies(self, tomorrow: bool=None):
+        ep_today = "/v2/achievements/daily"
+        ep_tomorrow = ep_today + "/tomorrow"
+
+        dailies_data = self.get_request(ep_tomorrow if tomorrow else ep_today, None)
+        return dailies_data
+
+    def get_daily_quest_details(self, ids, lang=None):
+        ep_daily_details = "/v2/achievements"
+        args = {"ids": ids}
+
+        if(lang is not None):
+            args["lang"] = lang
+
+        daily_details = self.get_request(ep_daily_details, args)
+        return daily_details
+
+class gw2_high_level_api_client:
+    def __init__(self):
+        self.rest_client = gw2_api_client()
+
+    def get_daily_achievements(self, tomorrow, category, lang=None):
+        api_client = gw2_api_client()
+        dailies = api_client.get_dailies(True if (tomorrow == "tomorrow") else False)
+
+        daily_ids = []
+        for daily in dailies[category]:
+            daily_ids.append(str(daily["id"]))
+
+        daily_details = api_client.get_daily_quest_details(",".join(daily_ids), self.locale)
+
+        achievement_list = []
+        for dailyd in daily_details:
+            achievement_list.append(achievement(dailyd["id"], dailyd))
+        return achievement_list
+
+    class achievement_tier:
+        def __init__(self, json=None):
+            self.count = 0
+            self.points = 0
+
+            if(json is not None):
+                self.load_from_json(json)
+
+        def load_from_json(self, json: dict):
+            self.count = json["count"]
+            self.points = json["points"]
+
+    class achievement:
+        def __init__(self, id, json=None):
+            self.id = id
+            self.name = None
+            self.description = None
+            self.requirement = None
+            self.locked_text = None
+            self.type = None
+            self.flags = []
+            self.tiers = []
+            self.rewards = []
+
+            if(json is not None):
+                self.load_from_json(json)
+
+        def load_from_json(self, json: dict):
+            self.id = json["id"]
+            self.name = json["name"]
+            self.description = json["description"]
+            self.requirement = json["requirement"]
+            self.locked_text = json["locked_text"]
+            self.type = json["type"]
+            self.flags = json["flags"]
+            self.tiers = []
+            for tier in json["tiers"]
+                self.tiers.append(achievement_tier(tier))
+            self.rewards = []
 
 class gw2:
     def __init__(self, bot):
@@ -53,20 +165,13 @@ class gw2:
             await self.bot.say(self.strings["invalid_parameter"].format(category, "category"))
             return
 
-        api_client = gw2_api_client()
-        dailies = api_client.get_dailies(True if (tomorrow == "tomorrow") else False)
-
-        daily_ids = []
-        for daily in dailies[category]:
-            daily_ids.append(str(daily["id"]))
-
-        daily_details = api_client.get_daily_quest_details(",".join(daily_ids), self.locale)
+        api_client = gw2_high_level_api_client()
+        daily_list = api_client.get_daily_achievements(True if (tomorrow == "tomorrow") else False, category, self.locale)
 
         em = discord.Embed(title=self.strings["daily_quests_embed_title"].format(category))
 
-        for i in range(len(dailies[category])):
-            req_lvl = dailies[category][i][""]
-            em.add_field(name=daily_details[i]["name"], value="".format(daily_details[i]["requirement"]), inline=False)
+        for quest in daily_list:
+            em.add_field(name=quest.name, value="".format(quest.requirement), inline=False)
 
         await self.bot.say(embed=em)
 
