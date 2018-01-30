eb44f380db539a19db50de8efacfd5ad3cfeeb09
Antoine Mazeas <antoine@karthanis.net>
2018-01-30 23:56:59 +0900

    Removed gw2api

--- gw2/gw2api/model/Title.py
+++
@@ -1,9 +0,0 @@
-from gw2api.model.LoadableObject import LoadableObjectBase
-
-class Title(LoadableObjectBase):
-    def __init__(self, id):
-        self.id = id
-        self.name = None
-
-    def _populate_inner(self, json):
-        self.name = json.get("name", None)
